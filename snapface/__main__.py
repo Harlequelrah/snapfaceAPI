3cb521a3ab443e8589b608d79fc13c0dbef85aeb
Harlequelrah <degbovimax19@gmail.com>
2024-11-09 01:50:35 +0000

    ajout d'un middleware pour accepter les requetes venants de "http://localhost:4200"

--- snapface/__main__.py
+++ snapface/__main__.py
@@ -1,14 +1,20 @@
 from sqlalchemy.orm import Session
 from fastapi import FastAPI, Depends, HTTPException, status
 import uvicorn
-from settings.secret import authentication
+from fastapi.middleware.cors import CORSMiddleware
 from settings.database import engine
 from snapfaceapi import model
 from snapfaceapi.route import app_facesnap
 
 app = FastAPI()
 app.include_router(app_facesnap)
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:4200"],  # Origines autorisées
+    allow_credentials=True,
+    allow_methods=["*"],  # Permet toutes les méthodes HTTP (GET, POST, etc.)
+    allow_headers=["*"],  # Permet tous les en-têtes
+)
 model.Base.metadata.create_all(bind=engine)
 
 
